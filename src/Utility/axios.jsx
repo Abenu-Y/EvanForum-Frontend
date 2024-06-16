4c17fbdda3478065ef6d378407de45d62552794b
Abenezer Y <abeneyoseph@gmail.com>
2024-06-17 01:34:41 +0300

    Evan Froum Deploy

--- src/Utility/axios.jsx
+++ src/Utility/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:'http://localhost:1234/api',
-    baseURL:'https://evanforum-backend-2.onrender.com'
+    baseURL:'https://evanforum-backend-2.onrender.com/api'
 
 })
 
